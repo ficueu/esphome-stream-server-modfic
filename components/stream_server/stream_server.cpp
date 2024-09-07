30802cbe44a58b4f1f4423f5716da7f5a294719f
ficueu <strona@ficu.eu>
2024-09-07 20:55:27 +0200

    Update stream_server.cpp

--- components/stream_server/stream_server.cpp
+++ components/stream_server/stream_server.cpp
@@ -42,7 +42,7 @@ void StreamServerComponent::loop()
 {
     this->accept();
     this->read();
-    this->flush();
+    this->ss_flush();
     this->write();
     this->cleanup();
 }
@@ -139,7 +139,7 @@ void StreamServerComponent::read()
     }
 }
 
-void StreamServerComponent::flush()
+void StreamServerComponent::ss_flush()
 {
     ssize_t written;
     this->buf_tail_ = this->buf_head_;
